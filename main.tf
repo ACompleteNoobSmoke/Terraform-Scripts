f854a1a822d635d2a3d577eb2821c0967001248f
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2024-04-23 08:25:48 -0500

    New server

--- main.tf
+++ main.tf
@@ -20,3 +20,12 @@ resource "aws_instance" "based_server" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_instance" "based_server_2" {
+  ami           = "ami-0b0ea68c435eb488d"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "BasedServer"
+  }
+}
